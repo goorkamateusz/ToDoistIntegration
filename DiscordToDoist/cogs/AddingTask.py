2fecd85f88fbc234c5141936bbd0f8beb63c37f5
Górka Mateusz <maatiug@gmail.com>
2022-10-14 14:30:20 +0200

    create thread

--- DiscordToDoist/cogs/AddingTask.py
+++ DiscordToDoist/cogs/AddingTask.py
@@ -1,18 +1,23 @@
+from discord.ext.commands.context import Context
 from ApiClient import ApiClient
 from Container import Container
 import discord
 from discord.ext import commands
-from todoist_api_python.api import TodoistAPI
 
 
 class AddingTask(commands.Cog):
     def __init__(self, bot: discord, api: ApiClient = Container.apiClient):
         self.bot = bot
 
-    @commands.command("add")
+    @commands.command("echo")
     async def add(self, ctx, *args):
         out = " ".join(args)
-        await ctx.send(f"ToDo {out}")
+        await ctx.send(f"[{out}]")
+
+    @commands.command("thread")
+    async def add(self, ctx: Context, *args):
+        name = " ".join(args) if args else "No name"
+        await ctx.channel.create_thread(name=name, auto_archive_duration=60, message=ctx.message)
 
 
 async def setup(bot):
