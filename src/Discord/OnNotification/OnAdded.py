076b6f45a3c84f1d8250acf365d139bbd3a40df5
Górka Mateusz <maatiug@gmail.com>
2022-10-30 21:01:46 +0100

    Detect task which have been added already

--- src/Discord/OnNotification/OnAdded.py
+++ src/Discord/OnNotification/OnAdded.py
@@ -1,7 +1,8 @@
-import discord
+import logging
 from typing import Any, Dict
 from src.Database.TaskMsg import TaskMsg
-from src.Discord.DiscordClient import OnNotificationComponent
+from src.Discord.DiscordComponent import OnNotificationComponent
+from src.Discord.Reactions import managed_msg_reaction
 from src.config import temp_channel_id
 
 
@@ -10,9 +11,12 @@ class OnAdded(OnNotificationComponent):
         event_data: Dict[str, Any] = event["event_data"]
         task_id = event_data["id"]
 
-        client: discord.Client = self.client
+        if self.db.find_one({"todoist_task_id": task_id}):
+            logging.info(f"Task have been added yet | {task_id}")
+            return
+
         channel_id = int(temp_channel_id)
-        channel = client.get_channel(channel_id)
+        channel = self.client.get_channel(channel_id)
         content = event_data["content"]
 
         msg = await channel.send(content)
@@ -27,4 +31,4 @@ class OnAdded(OnNotificationComponent):
         self.db.insert_one(entity.to_dict())
 
         communicate = "Dodano zadanie przez aplikację"
-        await self.report(entity, communicate)
+        await self.report(entity, communicate, managed_msg_reaction)
