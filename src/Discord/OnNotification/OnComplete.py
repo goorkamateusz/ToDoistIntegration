3aa55d50546d69c3d19289f4253f2fb878fa58d6
Górka Mateusz <maatiug@gmail.com>
2022-11-03 20:28:30 +0100

    OnComplete form multiple channels

--- src/Discord/OnNotification/OnComplete.py
+++ src/Discord/OnNotification/OnComplete.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Any, Dict
+from typing import Any, Dict, List
 from src.Database.Entities import TaskEntity
 from src.Discord.Reactions import done_reaction
 from src.Discord.DiscordClient import OnNotificationComponent
@@ -10,14 +10,11 @@ class OnComplete(OnNotificationComponent):
         event_data: Dict[str, Any] = event["event_data"]
 
         id = event_data["id"]
-        entity: TaskEntity = self.db.find_one(
+        entities: List[TaskEntity] = self.db.find(
             TaskEntity, {"todoist_task_id": id})
 
-        if entity:
-            communicate = "Zamknięto zadanie w aplikacji ToDoist"
+        for entity in entities:
             await self.report(entity,
-                              communicate=communicate,
-                              reaction=done_reaction)
+                              "Zamknięto zadanie w aplikacji ToDoist",
+                              done_reaction)
             logging.info(f"Notification processed successfully | {id}")
-        else:
-            logging.info(f"Task not registered | {id}")
